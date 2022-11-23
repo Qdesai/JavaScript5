e184db4b99af65bce73124b8ac72977b394da659
Qdesai <qaasimdesai786@gmail.com>
2022-11-23 00:15:03 -0500

    added the navigation bar

--- trimhouseapp/src/App.js
+++ trimhouseapp/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Home from './Home';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Navbar/>
     </div>
   );
 }
