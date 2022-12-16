8c1e1076db39281c48f62d673110f6bcaa67298c
Qdesai <qaasimdesai786@gmail.com>
2022-12-16 00:46:00 -0500

    updated the carousel and it works

--- trimhouseapp/src/Components/Caro.js
+++ trimhouseapp/src/Components/Caro.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Carousel } from 'antd';
+import im1 from "../Images/im1.jpg";
+import im2 from "../Images/im2.jpg";
+import im3 from "../Images/im3.jpg";
+import im4 from "../Images/im4.jpg";
 
 const contentStyle = {
   height: '300px',
@@ -7,24 +11,29 @@ const contentStyle = {
   lineHeight: '160px',
   textAlign: 'center',
   background: '#364d79',
+  imgposition: 'center',
+  imgrepeat: 'no-repeat',
+  imgsize: 'cover',
+  margin: '0 auto',
 };
 
 const Caro = () => {
 
 return(
     <>
+    
     <Carousel autoplay>
     <div>
-      <img src=''></img>
+    <img src={im1} style={contentStyle} alt="no image" />  
     </div>
     <div>
-    <img src=''></img>
+    <img src={im2} style={contentStyle} alt="no image" />
     </div>
     <div>
-    <img src=''></img>
+    <img src={im3} style={contentStyle} alt="no image" />
     </div>
     <div>
-    <img src=''></img>
+    <img src={im4} style={contentStyle} alt="no image" />
     </div>
   </Carousel>
     </>
